302c37e16d96d2dff1e52f9fa4c5292b6b885604
x404 <kronos2003@gmail.com>
2017-02-22 21:33:59 +0200

    Добавлена шапка и футер

--- js/engine.js
+++ js/engine.js
@@ -1,30 +1,5 @@
 $(document).ready(function(){
-	$('.categories a img').hover(
-		function(){$(this).stop().animate({opacity: 0.7}, 150);},
-		function(){$(this).delay(50).animate({opacity: 1}, 50);}
-	);
 
-
-	$(".slider ul").carouFredSel({
-	    auto    : {
-	    	play: true,
-	        items           : 1,
-	        duration        : 5000
-	    },
-		scroll      : {
-	        fx          : "crossfade"
-	    },
-		pagination  : ".pagination"			
-	});
-
-	$("#foo1").owlCarousel({
-		items : 1,
-		pagination : true,
-		navigation : true,
-		singleItem : true,
-		navigationText: ["", ""]
-	});	
-});
 
 // =заглушка для IE
 //event listener: DOM ready
