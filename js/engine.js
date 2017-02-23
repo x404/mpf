ca8714756b41068a391d674d36863c74d47458c1
x404 <kronos2003@gmail.com>
2017-02-23 20:18:08 +0200

    Добавлено всплывающее окно "Contact Us"

--- js/engine.js
+++ js/engine.js
@@ -21,6 +21,35 @@ $(document).ready(function(){
 	});
 
 
+
+
+	// validation
+	$('#contactus-form .btn-submit').click(function(){
+		$('#contactus-form').submit();
+		return false;
+	});
+
+	errorTxt = 'Ошибка отправки';
+	thankTxt = '<div class="thank"> <div class="inner"> <button class="close" type="button"></button> <p class="name">Dear Antotn Gorodetskiy</p> <p>Thank you for submitting your question to the My Power Farm. <br> Your satisfaction is our goal!</p> <p>Our manager will contact you regarding<br>your request as soon as possible!</p> <p><strong>Sincerely, <br>My Power Farm</strong></p> </div> </div>';
+	$('#contactus-form').validate({
+		submitHandler: function(form){
+			strSubmit=$(form).serialize();
+			$.ajax({type: 'POST',url: '/ajax/callback.ajax.php',data: strSubmit,
+				success: function(){
+				}
+			}).fail(function(error){
+					$('#contactus').fadeOut();
+					$('body').append(thankTxt);
+					startClock('contactus-form');
+				// alert(errorTxt)
+			});
+		}
+	});
+})
+
+$(document).on('click','.thank .close', function(e){    
+    e.preventDefault();
+    var $this = $(this);
 })
 
 
@@ -49,3 +78,34 @@ addLoadEvent(function(){
 	})
 });
 // =/заглушка для IE
+
+
+
+
+var timer;
+var sec = 4;
+
+function showTime(form){
+	sec = sec-1;
+	if (sec <=0) {
+		stopClock();
+		if (form == 'contactus-form'){ 
+			$('.thank').fadeOut('normal', function(){
+				$('.thank').remove();
+				$('#contactus-form .form-control').val('');
+				$('#contactus').modal('hide');
+			})
+		}
+	}
+}
+
+function stopClock(){
+	window.clearInterval(timer);
+	timer = null;
+	sec = 4;
+}
+
+function startClock(form){
+	if (!timer)
+	timer = window.setInterval("showTime('"+form+"')",1000);
+}
